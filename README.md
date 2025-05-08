06ead2e72bdd00eead5b9d02f2d7880b76e0e973
sagumad <samuelmartinezguzman830@gmail.com>
2025-05-08 16:07:34 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## ejercicio de logica condicional 
+
+realizar un ejercicio de logica condicional y montarlo a git con almenos 2 commit que registren el progreso 
+
+## requisitos para inscripcion a gym 
+
+-- debe tener almenos 15 o mas años de edad
+-- debe haber pagado la inscripcion
+-- debe haber pagado la mitad de la mensualidad
+-- debe estar afiliado a una casa de compensacion
---
+++ src/App.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("--Bienvenido a la inscripcion del gym--");
+            System.out.println("Ingrese su nombre: ");
+            String nombre = scanner.nextLine();
+            System.out.println("Ingrese su edad: ");
+            int edad = scanner.nextInt();
+            System.out.println("Ingrese su identificacion: ");
+            String identificacion = scanner.next();
+            System.out.println("Ingrese el valor de inscripcion a pagar: ");
+            double valorIns = scanner.nextDouble();
+            System.out.println("Ingrese el valor de la mensualidad a pagar: ");
+            double valorMens = scanner.nextDouble();
+
+            if (valorIns >= 50000 && valorMens >= 35000) {
+                System.out.println("El pago se ha realizado con exito.");
+            } else {
+                System.out.println("Error en el pago, por favor verifique los valores ingresados.");
+            }
+            if (identificacion.length() == 10) {
+                System.out.println("La identificacion es correcta esta afiliado a casa de compensacion.");
+                if (edad < 15) {
+                    System.out.println("No puede inscribirse, no cumple con el rewuisito de edad.");
+                } else {
+                    System.out.println("Nombre: " + nombre);
+                    System.out.println("Edad: " + edad);
+                    System.out.println("Identificacion: " + identificacion);
+                    System.out.println("Valor de inscripcion: " + valorIns);
+                    System.out.println("Valor de mensualidad: " + valorMens);
+                }
+                System.out.println("Gracias por inscribirse al gym, que tenga un buen dia.");
+            }
+        }
+
+    }
+}
